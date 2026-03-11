25fa8d70f4fde3768fbd359f5d0a9cd2ef6afa84
henriquebaz26 <h24henriquebaz@gmail.com>
2026-03-11 07:46:32 -0300

    feat: create OrderItem entity

---
+++ src/model/entities/OrderItem.java
@@ -0,0 +1,91 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class OrderItem implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private Integer quantity;
+	private Double price;
+	
+	private Order order;
+	private Product product;
+	
+	public OrderItem() {
+	}
+
+	public OrderItem(Integer id, Integer quantity, Double price, Order order, Product product) {
+		super();
+		this.id = id;
+		this.quantity = quantity;
+		this.price = price;
+		this.order = order;
+		this.product = product;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "OrderItem [id=" + id + ", quantity=" + quantity + ", price=" + price + ", order=" + order + ", product="
+				+ product + "]";
+	}
+
+}
