6ac754c354d756f12aae26052ed77645a9e780c9
henriquebaz26 <h24henriquebaz@gmail.com>
2026-03-11 07:34:45 -0300

    feat: create Product entity

---
+++ src/model/entities/Product.java
@@ -0,0 +1,101 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Product implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String name;
+	private String description;
+	private Double price;
+	private Integer stock;
+	
+	private Category categoryId;
+	
+	public Product() {
+	}
+
+	public Product(Integer id, String name, String description, Double price, Integer stock, Category categoryId) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.stock = stock;
+		this.categoryId = categoryId;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Integer getStock() {
+		return stock;
+	}
+
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+
+	public Category getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Category categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price + ", stock="
+				+ stock + ", categoryId=" + categoryId + "]";
+	}
+
+}
